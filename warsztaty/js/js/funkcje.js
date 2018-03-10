ea86b44ab230369d07ff700ce50bf3c4b70ef388
Malgorzata Gryniewicz <gosiunka@gmail.com>
2018-03-10 16:57:47 +0100

    obliczenia podatku

---
+++ warsztaty/js/js/funkcje.js
@@ -0,0 +1,29 @@
+//funkcja bez parametrow
+function wyswietlWKonsoli(){
+    console.log ("Malgorzata Gryniewicz");
+}
+wyswietlWKonsoli();
+
+//funkcja z parametrami
+function wyswietlWKonsoliParam( imie, nazwisko)
+{console.log( imie + " " + nazwisko);
+}
+wyswietlWKonsoliParam("XXX", "XXXX");
+wyswietlWKonsoliParam("AAA", "AAA");
+wyswietlWKonsoliParam("CCC", "AAA");
+
+//zwracanie wartosci przez funkcję
+function obliczPodatek( kwota, procentPodatek ){
+ var wynik = kwota * (procentPodatek / 100);
+return wynik.toFixed(2);   
+    
+}
+var obliczenia1 = obliczPodatek(4990, 23);
+console.log ("wysokość obliczonego podatku wynosi: "+obliczenia1);
+
+
+
+var w = obliczPodatek(1000,10);
+var x = obliczPodatek(2500,30);
+var z = obliczPodatek(3000,10);
+console.log(w,x,z);
