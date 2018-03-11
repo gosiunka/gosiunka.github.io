f17e799c4842bdef2eebee9d49bd3ae91dedcf8f
Malgorzata Gryniewicz <gosiunka@gmail.com>
2018-03-11 14:48:52 +0100

    operatory cd

---
+++ warsztaty/javascript/operatory/js/operatory.js
@@ -0,0 +1,68 @@
+//operatory arytmetyczne
+var liczba1 = 10;
+var liczba2 = 5;
+//dodawanie
+var wynik = liczba1 + liczba2;
+//mnozenie
+wynik = liczba1 * liczba2;
+//modulo
+wynik = liczba1 % liczba2;
+console.log(wynik);
+
+//inkrementacja
+
+liczba2++;
+liczba2++;
+liczba2--;
+
+console.log(liczba2);
+
+//operatory przypisania
+var liczba3= 200;
+var liczba4= 100;
+var liczba7= 7;
+
+liczba3 += 200;
+console.log( liczba3 )
+liczba7 %= 2;
+console.log( liczba7 )
+
+//operatory porownania
+
+var liczba8 = 42;
+var liczba9 = 10;
+
+if( liczba8 == liczba9 ){
+    console.log("warunek spelniony, czyli true");
+} else {
+ console.log ("Warunek nie spełniony, czyli false");
+    }
+//operatory logiczne
+
+
+
+var liczba10 =20;
+var liczba11 =4;
+var czyWiosna ="tak";
+
+if ( liczba10 < liczba11 && czyWiosna == "tak")
+    { console.log ("warunek spełniony");
+    }else {
+        console.log ("warunek nie spełniony");
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
