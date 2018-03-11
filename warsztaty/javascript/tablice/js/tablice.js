bea5411c58e329e0ef0b8aa41e4a22fb48fb8efa
Malgorzata Gryniewicz <gosiunka@gmail.com>
2018-03-11 11:40:29 +0100

    tablice

---
+++ warsztaty/javascript/tablice/js/tablice.js
@@ -0,0 +1,46 @@
+var imiona = ['Malgosia', 'Agnieszka', 'Marta', 'Sandra', 'Joanna', 'Anna'];
+console.log( imiona[0], imiona[3] );
+
+//DODAWANIE  ELEMENTOW TABLICY NA KONCU 
+//imiona[6] = "Mikolaj";
+//imiona[7] = "Kinga";
+//console.log( imiona );
+imiona.push("Kamil","Leon");
+imiona.push("Marcin");
+
+//usuwanie elemntow  TABLICY z koncua 
+imiona.pop();
+imiona.pop();
+
+//DODAWANIE ELEMENTOW  NA POCZATKU
+imiona.unshift("Sebastian");
+imiona.unshift("Piotrek");
+
+//USUWANIE ELEMENTOW z poczatku TABLICY
+imiona.shift();
+//sprawdza ktory element usunelismy
+//var usunietyElement = imiona.shit();
+//console.log ( usunietyElement );
+console.log( imiona );
+
+// sprawdzenie "dlugosci" tablicy 
+var iloscElementowTablicy = imiona.length;
+console.log( iloscElementowTablicy);
+
+//rozbijanie TABLICY na ciag tekstowy
+var elementyTablicyString = imiona.join(", ");
+//console.log (elementyTablicyString);
+
+//odwrocenie TABLICY
+//imiona.reverse();
+//console.log( imiona);
+
+//sortowanie TABLICY"
+imiona.sort();
+console.log(imiona);
+
+//SPRAWDZENIE czy wartość w TABLICY istnieje
+console.log (imiona.indexOf( "Sandra" ));
+
+var newImiona = imiona.slice (1,2);
+console.log ( newImiona);
