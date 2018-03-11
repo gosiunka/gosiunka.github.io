45532c17e479c0513b5386a8c5c823160cc226ed
Malgorzata Gryniewicz <gosiunka@gmail.com>
2018-03-11 16:16:27 +0100

    petle

---
+++ warsztaty/javascript/petle/js/petle.js
@@ -0,0 +1,53 @@
+//petla for
+//for( var i = 1; i <= 10; i++ ){
+//console.log("Iteracja nr " + i);
+//}
+
+//var tablica =[ "volvo", "mazda", "honda", "Audi"];
+
+//for ( var i =0; i< tablica.length; i++) {
+// console.log( tablica[i]);}
+//}
+
+//petla forEach
+
+//tablica.forEach( function( element, index ){
+ //   console.log( "Index: "+ index +" Element: " + element);
+//});
+
+//petla while
+
+//var iter = 1;
+//while( iter <25){
+//  console.log("while iteracja nr" + iter);
+//  iter++;
+//}
+
+//petla "do" najpierw robi, potem sprawdza warunek
+//var iter = 28;
+//do {
+//console.log( "while iteracja nr" +iter);
+//iter++;
+//} while (iter <=25)
+
+//break i continue
+
+for (var i= 1; i< 15; i++){
+    if(1>6){
+    break;
+    }else{
+        console.log("obrot petli nr" +i);
+    }
+    }
+
+for (var i= 1; i< 15; i++){
+    if(i == 4 || i==6 || i== 10) {
+    continue;
+    }else{
+        console.log("iteracja  nr" +i);
+    }
+    }
+    
+    
+    
+
