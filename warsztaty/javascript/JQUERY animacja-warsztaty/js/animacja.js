b1649dab8409a4e4b366cab91c66da7216bf3deb
Malgorzata Gryniewicz <gosiunka@gmail.com>
2018-03-24 09:24:09 +0100

    poprawki

---
+++ warsztaty/javascript/JQUERY animacja-warsztaty/js/animacja.js
@@ -0,0 +1,59 @@
+$(function(){
+    
+    var slideShow = $(".slide-show");
+    var slideCount = $(".single-slide").lenght;
+    var slideWidth = 100/slideCount;
+    var slideIndex = 0;
+    
+    // ustawiam szerokosci .slide-show
+    $ (slideShow).css({
+        "width": slideWidth  + "00%"
+    });
+   
+    
+function.SlideMove(ind){ 
+    if(ind<0 || ind >(sideCount-1)){
+        return;
+    }
+     
+    
+    
+    
+    
+    
+    
+      });
+
+    
+    
+    
+    
+    
+    slideShow.find( ".single-slide" ).each( functionion (ind){
+     $(this).css({
+        
+    })                                      
+                                           }
+    { "left": slideWidth  
+     slideIndex/
+        
+//$(xx)each(function(index)
+
+
+
+)        
+                                                 
+                                                 
+                                                 
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+} );
--- warsztaty/javascript/JQUERY animacja/js/animacja.js
+++ warsztaty/javascript/JQUERY animacja/js/animacja.js
@@ -1,20 +1,20 @@
-$(function(){
-    $(".mylink").on("click", function(event){
-    event.preventDefault();
-    $("body").css({backgroundColor: "green"});
-});
-    
-    
-    
-//podpiecie kilku zdarzeń po najechaniu myszka podkresla tekst po zdjeciu nie
-$("h1").on({
-    "mouseenter": function(){
-        $(this).css({textDecoration : "underline"});
-    },
-    "mouseleave" : function() {
-        $(this).css({textDecoration : "none"});
-    }
-    
-});
-    
+var btn = $("#animate");
+//druga metoda on nowoczesniejsza
+btn.on("click", function () {
+
+    //$("h1").hide(4000).show("fast");
+    //$("h1").fadeOut(3000).fadeIn("slow");
+    //$("h1").slideUp(600).slideDown(400);
+
+    $("h1").animate(
+        {
+           "left": "100px",
+           "top": "100px",
+            }, 4000,
+        function(){
+            alert("koniec animacji");
+        }
+
+);
+
 });
