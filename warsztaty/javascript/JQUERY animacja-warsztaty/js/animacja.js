f116f81f3c912f92e7de26dd1258b23e1c502563
Malgorzata Gryniewicz <gosiunka@gmail.com>
2018-03-24 16:31:07 +0100

    slider

--- warsztaty/javascript/JQUERY animacja-warsztaty/js/animacja.js
+++ warsztaty/javascript/JQUERY animacja-warsztaty/js/animacja.js
@@ -1,59 +1,51 @@
-$(function(){
-    
-    var slideShow = $(".slide-show");
-    var slideCount = $(".single-slide").lenght;
-    var slideWidth = 100/slideCount;
-    var slideIndex = 0;
-    
-    // ustawiam szerokosci .slide-show
-    $ (slideShow).css({
-        "width": slideWidth  + "00%"
-    });
-   
-    
-function.SlideMove(ind){ 
-    if(ind<0 || ind >(sideCount-1)){
-        return;
-    }
-     
-    
-    
-    
-    
-    
-    
-      });
+$(function () {
+            var slideShow = $(".slide-show");
+            var slideCount = $(".single-slide").length;
+            var slideWidth = 100 / slideCount;
+            var slideIndex = 0;
 
+            // ustawiam szerokosci .slide-show moze byc z klamra{} i :
+            slideShow.css(
+                "width", slideCount * 100 + "%");
+
+            slideShow.find(".single-slide").each(function (index) {
+                $(this).css({
+                    "width": slideWidth + "%",
+                    "margin-left": index * slideWidth + "%"
+                });
+            });
+            $(".prev-slide").click(function () {
+                slide(slideIndex - 1);
+            });
     
-    
-    
-    
-    
-    slideShow.find( ".single-slide" ).each( functionion (ind){
-     $(this).css({
-        
-    })                                      
-                                           }
-    { "left": slideWidth  
-     slideIndex/
-        
-//$(xx)each(function(index)
+            $(".next-slide").click(function () {
+                slide(slideIndex + 1);
+            });
 
 
 
-)        
-                                                 
-                                                 
-                                                 
-    });
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-} );
+
+            function slide(newSlideIndex) {
+                if (newSlideIndex < 0 || newSlideIndex > slideCount - 1)
+                { 
+                    return;
+                }
+
+            var slideCaption = slideShow.find(".slide-caption").eq(newSlideIndex);
+            slideCaption.hide();
+
+
+            var newMarginLeft = (newSlideIndex * (-100)) + "%";
+
+            slideShow.animate({
+                    "margin-left": newMarginLeft
+                },
+                800,
+                function() {
+                    slideIndex = newSlideIndex;
+                    slideCaption.fadeIn();
+
+                });
+
+        };
+});
