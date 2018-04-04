97128560a00378d841cfdee694d0ef085860eebb
Malgorzata Gryniewicz <gosiunka@gmail.com>
2018-04-04 20:35:26 +0200

    poprawka zadania

--- warsztaty/javascript/AJAX zadanie domowe/js/funkcje.js
+++ warsztaty/javascript/AJAX zadanie domowe/js/funkcje.js
@@ -4,6 +4,8 @@ function pobierzDane(event) {
         "https://akademia108.pl/kurs-front-end/ajax/1-pobierz-dane-programisty.php",
         wrzucDanedoHtml)
 }
+
+$('body').append('<div id= dane-programisty></div>');
 function wrzucDanedoHtml(dane){
     $('#dane-programisty').append("<p>imie:" + dane.imie+"</p>");
     $('#dane-programisty').append("<p>nazwisko:" + dane.nazwisko+"</p>");
